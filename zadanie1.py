e41a9b8289b12985999641db5fb5856d750fc4c2
aregdz <144221910+aregdz@users.noreply.github.com>
2024-05-21 14:34:01 +0300

    Update zadanie1.py

--- zadanie1.py
+++ zadanie1.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Создать класс Triad (тройка чисел); определить методы изменения полей и вычисления
+#суммы чисел. Определить производный класс Triangle с полями-сторонами. Определить
+#методы вычисления углов и площади треугольника.
+
+# релизовать новый метод класса, для подсчёта углов, данный метод должен вызываться из других методов, в которых это требуется
+# добавить if __name__ == "__main__":
+
 import math
-# создать метод класса и реалтзовать внутри math.sqrt(p * (p - self.x) * (p - self.y) * (p - self.z))
 
 class Triad:
     def __init__(self, x=1, y=1, z=1):
@@ -33,31 +42,38 @@ class Triangle(Triad):
     def __init__(self, x, y, z):
         super().__init__(x, y, z)
 
+    @staticmethod
+    def calculate_angle(a, b, c):
+        # Метод для вычисления угла
+        return math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
+
     def s(self):
+        # Вычисление площади треугольника по формуле Герона
         p = (self.x + self.y + self.z) / 2
         return math.sqrt(p * (p - self.x) * (p - self.y) * (p - self.z))
 
     def angle_A(self):
-        a, b, c = self.x, self.y, self.z
-        return math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
+        # Вычисление угла A
+        return self.calculate_angle(self.x, self.y, self.z)
 
     def angle_B(self):
-        a, b, c = self.x, self.y, self.z
-        return math.degrees(math.acos((c**2 + a**2 - b**2) / (2 * c * a)))
+        # Вычисление угла B
+        return self.calculate_angle(self.y, self.z, self.x)
 
     def angle_C(self):
-        a, b, c = self.x, self.y, self.z
-        return math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b)))
+        # Вычисление угла C
+        return self.calculate_angle(self.z, self.x, self.y)
 
     def __str__(self):
-        return f"x = {self.x};  y = {self.y};  z={self.z}"
+        return f"x = {self.x};  y = {self.y};  z = {self.z}"
 
 
-j = Triangle(25, 23, 35)
-print(j.s())
-print(j.x_get())
-j.y_set(34)
-print(j.angle_A())
-print(j.angle_B())
-print(j.angle_C())
-print(j)
+if __name__ == "__main__":
+    j = Triangle(25, 23, 35)
+    print(f"Площадь треугольника: {j.s()}")
+    print(f"x: {j.x_get()}")
+    j.y_set(34)
+    print(f"Угол A: {j.angle_A()}")
+    print(f"Угол B: {j.angle_B()}")
+    print(f"Угол C: {j.angle_C()}")
+    print(j)
